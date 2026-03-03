fc1d2b3e88be3463fe8c732ee9ca94a513b37b45
yurabiryukov <smile19x@yandex.ru>
2026-03-03 16:56:34 +1000

    Auth checks

--- tests/test_01_home_page.py
+++ tests/test_01_home_page.py
@@ -1,16 +1,17 @@
 import allure
 from pages.home_page import HomePage
+from pages.auth_page import AuthPage
 from pages.catalog_page import CatalogPage
 
 
 @allure.description("Test guest can get in catalog page")
 def test_guest_can_get_in_catalog_page(browser):
     """
-    В данном тесте мы находим вкладку 'Каталог' после того,
-    как оказались на главной странице сайта https://upstore24.ru/, и нажимаем на нее,
-    далее проверяем, произошел ли переход, через сравнение cp.main_word и слова 'Каталог'
-    в методе assert_main_word_and_result.
-    Если тест проходит assert, то выводится принт в консоль и сохраняется скриншот.
+    В данном тесте происходит нажатие на кнопку входа в учетную запись, заполняются поля с почтой и паролем на странице
+    авторизации, нажимается 'Войти'. Проверка, удалось ли авторизоваться, через сравнение ap.main_word и словосочетания
+    'История заказов' в методе assert_main_word_and_result. Возврат на главную страницу, нажатие на 'Каталог'
+    и переход на страницу каталога.
+    Если тест проходит assert-ы, то выводятся принты в консоль и сохраняются скриншот.
     """
     link = 'https://upstore24.ru/'
     browser.get(link)
@@ -18,6 +19,15 @@ def test_guest_can_get_in_catalog_page(browser):
 
     hp = HomePage(browser)
     hp.select_open_window_close_btn()
+    hp.click_auth_btn()
+
+    ap = AuthPage(browser)
+    ap.fill_the_inputs()
+    ap.assert_main_word_and_result(ap.get_main_word(ap.main_word), 'История заказов')
+    print('Мы вошли в учетную запись!')
+    ap.get_screenshot()
+    ap.click_logo_btn()
+
     hp.select_catalog_btn()
 
     cp = CatalogPage(browser)
