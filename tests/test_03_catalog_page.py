2321b248c655d0d11d89f60c40a362ffbf384fd9
yurabiryukov <smile19x@yandex.ru>
2026-04-28 16:42:01 +1000

    Add search test

--- tests/test_03_catalog_page.py
+++ tests/test_03_catalog_page.py
@@ -1,4 +1,4 @@
-import allure
+import allure, pytest, time
 from selenium.webdriver.chrome.webdriver import WebDriver
 from pages.catalog_page import CatalogPage
 from pages.gadgets_page import GadgetsPage
@@ -9,9 +9,19 @@ def test_guest_can_get_in_gadgets_page(browser: WebDriver, catalog_page: Catalog
 
     catalog_page.visit('https://upstore24.ru/collection/all')
 
-    catalog_page.select_open_window_close_btn()
     catalog_page.select_gadgets_btn()
 
     gadgets_page.assert_main_word_and_result(gadgets_page.get_main_word(gadgets_page.main_word), 'Гаджеты')
     print('Мы на странице с гаджетами!')
     gadgets_page.get_screenshot()
+
+
+@pytest.mark.parametrize('product_name', ('apple', 'фззду', '  xiaomi  ', '////'))
+@allure.description("Test user can use search")
+def test_user_can_use_search(browser: WebDriver, catalog_page: CatalogPage, product_name: str):
+
+    catalog_page.visit('https://upstore24.ru/collection/all')
+
+    catalog_page.fill_search_field(product_name)
+    time.sleep(2)
+    catalog_page.check_search_works_as_expected(product_name)
